8b14fe9ba6939b63c3625236ece1dd6286154832
kanis <kanis@LAPTOP-2J02IN7J>
2023-08-22 08:39:03 +0530

    Half done

---
+++ Priority_Queue/src/practice/PriorityQueueException.java
@@ -0,0 +1,10 @@
+package practice;
+
+public class PriorityQueueException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -6507704290438494008L;
+
+}
---
+++ Priority_Queue/src/practice/PriorityQueueUse.java
@@ -0,0 +1,29 @@
+package practice;
+
+public class PriorityQueueUse {
+
+	public static void main(String[] args) throws PriorityQueueException {
+		// TODO Auto-generated method stub
+		
+		int arr[]= {5,1,9,2,0};
+		
+		for(int i=0; i<arr.length; i++) {
+//			pq.insert(arr[i]);
+		}
+		
+//		Priority_Queue pq= new Priority_Queue();
+//		
+//		int arr[]= {5,1,9,2,0};
+//		
+//		for(int i=0; i<arr.length; i++) {
+//			pq.insert(arr[i]);
+//		}
+//		
+//		while(!pq.isEmpty()) {
+//			System.out.print(pq.removeMin()+" ");
+//		}
+//		System.out.println();
+
+	}
+
+}
